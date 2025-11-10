f32296795c2aa1b0e7a982c3f49ccb45432acbed
Javier López <fjavierlh@gmail.com>
2025-11-10 12:22:22 +0100

    chore: create infrastructure layer

---
+++ src/core/user/infrastructure/user-rest-repository.ts
@@ -0,0 +1,21 @@
+import type { User } from "../domain/user.model";
+import type { UserRepository } from "../domain/user.repository";
+import type { UserDTO } from "./user.dto";
+
+export class UserRestRepository implements UserRepository {
+  async getAll(): Promise<User[]> {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/");
+    const usersData = await response.json();
+
+    return usersData.map((userData: UserDTO) => ({
+      id: String(userData.id),
+      name: userData.name,
+      email: userData.email,
+      city: userData.address.city,
+      website: userData.website,
+      company: userData.company?.name,
+      albumIds: [],
+      todoListIds: [],
+    }));
+  }
+}
